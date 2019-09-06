64b95b62c0e09f89a041c3ec1676c4641de0acb6
Colleen <colleenhiggins87@gmail.com>
2019-09-05 17:09:27 -0700

    searching drills

--- search-alg.js
+++ search-alg.js
@@ -1,4 +1,5 @@
 const BinarySearchTree = require('../DST-BST-colleen/BST')
+const Queue = require('../DSA-stack-n-queue-colleen/queue')
 /* 1. How many searches?
 Given a sorted list 3, 5, 6, 8, 11, 12, 14, 15, 17, 18 and using the recursive binary search algorithm, identify the sequence of numbers that each recursive call will search to try and find 8.
 
@@ -214,3 +215,60 @@ function inOrder(tree, arr=[]){
   }
 
   console.log(inOrder(tree))
+
+
+/*   6. Find the next commanding officer
+Suppose you have a tree representing a command structure of the Starship USS Enterprise.
+
+               Captain Picard
+             /                \
+    Commander Riker       Commander Data
+      /         \               \
+ Lt. Cmdr.   Lt. Cmdr.          Lt. Cmdr.
+ Worf        LaForge            Crusher
+   /                           /
+Lieutenant                  Lieutenant
+security-officer            Selar
+
+
+This tree is meant to represent who is in charge of lower-ranking officers. For example, Commander Riker is directly responsible for Worf and LaForge. People of the same rank are at the same level in the tree. However, to distinguish between people of the same rank, those with more experience are on the left and those with less on the right (i.e., experience decreases from left to right). Suppose a fierce battle with an enemy ensues. Write a program that will take this tree of commanding officers and outlines the ranking officers in their ranking order so that if officers start dropping like flies, we know who is the next person to take over command. */
+//reverse in Order---start on R side 
+
+function rankingTree(tree, values = []) {
+  const queue = new Queue(); // Assuming a Queue is implemented (refer to previous lesson on Queue)
+  const node = tree.root;
+  queue.enqueue(node);
+  while (queue.length) {
+      const node = queue.dequeue(); //remove from the queue
+      values.push(node.value); // add that value from the queue to an array
+
+      if (node.left) {
+          queue.enqueue(node.left); //add left child to the queue
+      }
+
+      if (node.right) {
+          queue.enqueue(node.right); // add right child to the queue
+      }
+  }
+
+  return values;
+}
+/* The share price for a company over a week's trading is as follows: [128, 97, 121, 123, 98, 97, 105]. If you had to buy shares in the company on a particular day, and sell the shares on a following day, write an algorithm to work out what the maximum profit you could make would be. */
+
+function buy(arr=[], start=0, end=arr.length-1, get=0, sell=0){
+  
+  if(arr[start] > arr[end]){
+    console.log('TRUE', arr[start], arr[end])
+    sell = arr[start]
+    return buy(arr, start + 1, end, get, sell)
+  }
+  if(arr[start] < arr[end]){
+    console.log("FALSE", arr[start], arr[end])
+    get = arr[start]
+    return buy(arr, start +1, end, get, sell)
+  }
+  return {get, sell}
+
+}
+let arraynum = [128, 97, 121, 123, 98, 97, 105]
+console.log(buy(arraynum))
